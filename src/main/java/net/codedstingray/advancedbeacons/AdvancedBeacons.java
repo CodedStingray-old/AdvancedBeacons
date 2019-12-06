155e20cc7f47021d118aefa66ae7eb5ec60c0bb3
CodedStingray <ray.kampmann@gmail.com>
2019-12-06 16:57:42 +0100

    Initial commit

---
+++ src/main/java/net/codedstingray/advancedbeacons/AdvancedBeacons.java
@@ -0,0 +1,17 @@
+package net.codedstingray.advancedbeacons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedBeacons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
