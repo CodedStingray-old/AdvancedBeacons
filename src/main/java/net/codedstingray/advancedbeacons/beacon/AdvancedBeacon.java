40a6134ebfc4ecd4a1e391cd1ff841d0ff2e24db
CodedStingray <ray.kampmann@gmail.com>
2019-12-30 02:04:00 +0100

    Beacon creation and registration

--- src/main/java/net/codedstingray/advancedbeacons/beacon/AdvancedBeacon.java
+++ src/main/java/net/codedstingray/advancedbeacons/beacon/AdvancedBeacon.java
@@ -4,10 +4,19 @@ import org.bukkit.Location;
 
 public class AdvancedBeacon {
 
+    private BeaconStructure structure;
+    private Location location;
 
+    public AdvancedBeacon(BeaconStructure structure, Location location) {
+        this.structure = structure;
+        this.location = location.clone();
+    }
 
     public Location getLocation() {
-        //TODO
-        return null;
+        return location.clone();
+    }
+
+    public BeaconStructure getStructure() {
+        return structure;
     }
 }
--- src/main/java/net/codedstingray/advancedbeacons/beacon/BeaconManager.java
+++ src/main/java/net/codedstingray/advancedbeacons/beacon/BeaconManager.java
@@ -4,24 +4,40 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+
 public class BeaconManager {
 
+    private static HashMap<Location, AdvancedBeacon> advancedBeacons = new HashMap<>();
+
     public static boolean createBeacon(Player player, Location location) {
+        if(advancedBeacons.get(location) != null) {
+            BeaconStructure str = advancedBeacons.get(location).getStructure();
+            player.sendMessage(ChatColor.AQUA + "There's already an advanced beacon in this location with tier " + str.getTier() + " and a power of " + str.getPower());
+            return false;
+        }
+
+        //structure validation
         BeaconStructure structure = BeaconUtilities.validateBeacon(location);
-
-        player.sendMessage(structure.toString());
-
-        if(structure.isValid()) {
-            player.sendMessage(ChatColor.AQUA + "Advanced beacon created with tier " + structure.getTier() + " and a power of " + structure.getPower());
-            return true;
+        if(structure.equals(BeaconStructure.invalid()) && !player.isSneaking()) {
+            player.sendMessage(ChatColor.RED + "Invalid advanced beacon structure. Sneak right-click to force advanced beacon creation.");
+            return false;
         }
 
-        if(structure.equals(BeaconStructure.invalid())) {
-            player.sendMessage(ChatColor.RED + "Invalid advanced beacon structure");
-        } else {
+        if(structure.equals(BeaconStructure.noBeacon())) {
             player.sendMessage(ChatColor.YELLOW + "Click on a beacon with a valid advanced beacon structure to create an advanced beacon.");
+            return false;
         }
 
-        return false;
+        //beacon registration
+        AdvancedBeacon beacon = new AdvancedBeacon(structure, location);
+        advancedBeacons.put(beacon.getLocation(), beacon);
+
+        if (structure.isValid()) {
+            player.sendMessage(ChatColor.AQUA + "Advanced beacon created with tier " + structure.getTier() + " and a power of " + structure.getPower());
+        } else {
+            player.sendMessage(ChatColor.AQUA + "Advanced beacon created with invalid structure");
+        }
+        return true;
     }
 }
