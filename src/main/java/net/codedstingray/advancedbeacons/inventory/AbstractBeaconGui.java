485f0c463e10e8b83782aa091d10aa6a3ac8eacd
CodedStingray <ray.kampmann@gmail.com>
2020-01-16 01:58:24 +0100

    Inventory Menu Content

--- src/main/java/net/codedstingray/advancedbeacons/inventory/AbstractBeaconGui.java
+++ src/main/java/net/codedstingray/advancedbeacons/inventory/AbstractBeaconGui.java
@@ -9,14 +9,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 public abstract class AbstractBeaconGui implements InventoryHolder, Listener {
 
-    protected Inventory inv;
+    protected Inventory inventory;
     protected AdvancedBeacon beacon;
 
     public AbstractBeaconGui(int size, String title, AdvancedBeacon beacon) {
-        inv = Bukkit.createInventory(this, size, title);
+        inventory = Bukkit.createInventory(this, size, title);
         initializeItems();
         this.beacon = beacon;
 
@@ -25,7 +26,7 @@ public abstract class AbstractBeaconGui implements InventoryHolder, Listener {
 
     @Override
     public Inventory getInventory() {
-        return inv;
+        return inventory;
     }
 
     public AdvancedBeacon getBeacon() {
@@ -33,17 +34,20 @@ public abstract class AbstractBeaconGui implements InventoryHolder, Listener {
     }
 
     public void openInventory(Player p) {
-        p.openInventory(inv);
+        p.openInventory(inventory);
     }
 
-    //TODO: for optimization
+    protected void setItem(int row, int column, ItemStack item) {
+        int index = 9 * row + column;
+        inventory.setItem(index, item);
+    }
+
+    //TODO: for optimization I can probably condense this into a sincle listener instead of one for each inventory
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if(event.getInventory() != inv) {
-            AdvancedBeacons.getInstance().getLogger().info("Not same inventory");
+        if(event.getInventory() != inventory) {
             return;
         }
-        AdvancedBeacons.getInstance().getLogger().info("Same inventory");
 
         handleInventoryClick(event);
     }
--- src/main/java/net/codedstingray/advancedbeacons/inventory/BeaconGuiMain.java
+++ src/main/java/net/codedstingray/advancedbeacons/inventory/BeaconGuiMain.java
@@ -13,7 +13,26 @@ public class BeaconGuiMain extends AbstractBeaconGui {
 
     @Override
     protected void initializeItems() {
-        inv.addItem(Data.getMenuItem("Main_Header"));
+        setItem(0, 4, Data.getMenuItem("Header_Main"));
+
+        for(int i = 0; i < 6; i++) {
+            setItem(i, 3, Data.getMenuItem("Spacer"));
+            setItem(i, 5, Data.getMenuItem("Spacer"));
+
+            setItem(i, 0, Data.getMenuItem("Effect_Placeholder"));
+            setItem(i, 6, Data.getMenuItem("Effect_Placeholder"));
+
+            setItem(i, 1, Data.getMenuItem("Min_Max_Placeholder"));
+            setItem(i, 2, Data.getMenuItem("Min_Max_Placeholder"));
+            setItem(i, 7, Data.getMenuItem("Min_Max_Placeholder"));
+            setItem(i, 8, Data.getMenuItem("Min_Max_Placeholder"));
+        }
+
+        setItem(1, 4, Data.getMenuItem("Iron_Indicator"));
+        setItem(2, 4, Data.getMenuItem("Gold_Indicator"));
+        setItem(3, 4, Data.getMenuItem("Emerald_Indicator"));
+        setItem(4, 4, Data.getMenuItem("Diamond_Indicator"));
+        setItem(5, 4, Data.getMenuItem("Nether_Star_Indicator"));
     }
 
     @Override
